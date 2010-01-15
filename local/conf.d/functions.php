5546eeac28cc96c4805cc353a474a607d32aa3c3
Jens Wille <jens.wille@uni-koeln.de>
2010-01-15 17:05:41 +0100

    Added local config.

---
+++ local/conf.d/functions.php
@@ -0,0 +1,44 @@
+<?php if (!defined('PmWiki')) exit();
+## Automatic Change Summary
+array_unshift($EditFunctions, 'ProvideDefaultSummary');
+function ProvideDefaultSummary($pagename, &$page, &$new) {
+  global $ChangeSummary, $DiffFunction;
+
+  if ($ChangeSummary || !function_exists(@$DiffFunction)) return;
+
+  $diff = $DiffFunction($new['text'], @$page['text']);
+  $difflines = explode("\n", $diff."\n");
+
+  $in = array(); $out = array();
+  foreach ($difflines as $d) {
+    if ($d == '' || $d[0] == '-' || $d[0] == '\\') continue;
+    if ($d[0] == '<' && count($out) < 10) {
+      $out[] = substr($d, 2);
+      continue;
+    }
+    if ($d[0] == '>' && count($in) < 10) {
+      $in[] = substr($d, 2);
+      continue;
+    }
+  }
+
+  $diff2 = '';
+  if (count($out) == 0) {
+    $out = $in; $diff2 = "[deleted] ";
+  }
+  foreach ($out as $s) {
+    $diff2 .= $s." ";
+  }
+
+  $ChangeSummary = str_replace(array("<", ">", "&", "\n"), array("&lt;", "&gt;", "&amp;", " "), $diff2);
+  $new['csum'] = $ChangeSummary;
+}
+
+function MakeTags($string) {
+  $tags = array_map(
+    create_function('$s', 'return "[[!" . trim($s) . "]]";'),
+    explode(" ", $string)
+  );
+
+  return "<div class='tags'>Tags: " . join(" | ", $tags) . "</div>";
+}
---
+++ local/conf.d/markup.php
@@ -0,0 +1,11 @@
+<?php if (!defined('PmWiki')) exit();
+## Tags/Categories directive
+Markup('tags', 'directives', '/\\(:tags (.*?):\\)/e', "MakeTags('$1')");
+
+## Include uploaded files (attachments)
+Markup('includeupload', 'directives',
+  '/\(:includeupload\s+(.+)\s*:\)/e',
+  "is_readable(\"$FarmD/uploads/$1\")
+    ? '<pre>  ' . Keep(str_replace(array('<', '>'), array('&lt;', '&gt;'), implode('  ', file('$BaseUrl/uploads/$1')))) . '</pre><div style=\"text-align: right; margin-top: 0.4em;\">[Source: $BaseUrl/uploads/$1]</div>'
+    : '<p class=\"wikimessage\">[File not found: $1]</p>'"
+);
