4a316a4c838972bd1e21c107a7665a212c356f7c
Jens Wille <jens.wille@uni-koeln.de>
2010-10-12 11:57:26 +0200

    Drop startbild.php...

--- startbild.php
+++
@@ -1,6 +0,0 @@
-<?php
-  header("Content-type: image/jpg");
-  $html = file_get_contents('http://prometheus-bildarchiv.de/?skin=1');
-  ereg('<div id="startbild"><img[^>]* src="([^"]*)"', $html, $capture);
-  echo file_get_contents('http://prometheus-bildarchiv.de/'.$capture[1])
-?>
