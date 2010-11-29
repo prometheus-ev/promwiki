c362c4b9c38417ace2d73da3286f8d7a628e5758
Jens Wille <jens.wille@uni-koeln.de>
2010-11-29 11:01:01 +0100

    local/conf.d/31_upload.php: Allow .sh files.

--- local/conf.d/31_upload.php
+++ local/conf.d/31_upload.php
@@ -3,11 +3,12 @@ $EnableUpload = 1;
 
 $UploadMaxSize = 1000000;
 
-$UploadExts['sample'] = 'text/plain';
-$UploadExts['diff']   = 'text/plain';
-$UploadExts['mpatch'] = 'text/plain';
-$UploadExts['rb']     = 'text/plain';
-$UploadExts['conf']   = 'text/plain';
 $UploadExts['cfg']    = 'text/plain';
+$UploadExts['conf']   = 'text/plain';
+$UploadExts['diff']   = 'text/plain';
 $UploadExts['lang']   = 'text/plain';
+$UploadExts['mpatch'] = 'text/plain';
 $UploadExts['pkg']    = 'text/plain';
+$UploadExts['rb']     = 'text/plain';
+$UploadExts['sample'] = 'text/plain';
+$UploadExts['sh']     = 'text/plain';
